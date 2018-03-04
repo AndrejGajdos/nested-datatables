da552adc404df6881ecf916736018ffd6766c5a5
Andrej Gajdos <andrej.gajdos@toptal.com>
2018-03-04 20:01:58 +0100

    adding show/hide event listeners

--- src/script/TableBuilder.js
+++ src/script/TableBuilder.js
@@ -11,161 +11,166 @@ import ArrowImg from '../img/arrow_right.png';
  * @param {Array.<Object>} tableDataInJson Data in Json format which are used for building table hierarchy
  */
 export default class TableBuilder {
-	constructor(tableContainerID, tableDataInJson){
-		 this.idParser = new IdParser(tableContainerID, "row");
-		 this.dataInJson = JSON.parse(JSON.stringify(tableDataInJson));
-		 this.table = "";
-		 this.tableIdString = "";
-	}
+  constructor(tableContainerID, tableDataInJson) {
+    this.idParser = new IdParser(tableContainerID, 'row');
+    this.dataInJson = JSON.parse(JSON.stringify(tableDataInJson));
+    this.table = '';
+    this.tableIdString = '';
+  }
 
-	getTable() {
-		return this.table;
-	}
+  getTable() {
+    return this.table;
+  }
 
-	getIdParser() {
-		return this.idParser;
-	}
+  getIdParser() {
+    return this.idParser;
+  }
 
-	/**
-	 * Build table from object arguments
-	 */
-	buildTable() {
+  /**
+   * Build table from object arguments
+   */
+  buildTable() {
+    let data = this.dataInJson;
 
-		let data = this.dataInJson;
+    this.tableIdString = '';
+    if (this.idParser.getPatternOccurrence() > 0) {
+      this.idParser.parse();
 
-		this.tableIdString = "";
-		if (this.idParser.getPatternOccurence() > 0) {
-			this.idParser.parse();
+      // set required data according to pattern occurrence in element id
+      for (let j = 0; j < this.idParser.getParsedIds().length; j++) {
+        let parsedId = this.idParser.getParsedIds()[j];
 
-			// set required data according to pattern occurence in element id
-			for (let j = 0; j < this.idParser.getParsedIds().length; j++) {
-				let parsedId = this.idParser.getParsedIds()[j];
+        // if parsedId is integer
+        if (/^\+?\d+$/.test(parsedId)) {
+          data = JSON.parse(JSON.stringify(data[parsedId].kids));
+        }
+      }
 
-				// if parsedId is integer
-				if (/^\+?\d+$/.test(parsedId)){
-					data = JSON.parse(JSON.stringify(data[parsedId].kids));
-				}
-			}
+      this.tableIdString = this.idParser.getElementId() + '_tab_0';
+    } else {
+      this.tableIdString = 'tab_0';
+    }
 
-			this.tableIdString = this.idParser.getElementId() + "_tab_0";
-		} else {
-			this.tableIdString = "tab_0";
-		}
+    let tableInHtml = '<div>';
+    tableInHtml += this.buildTableContent(data);
+    tableInHtml += '</div>';
+    this.table = tableInHtml;
+  }
 
-		let tableInHtml = "<div>";
-		tableInHtml += this.buildTableContent(data);
-		tableInHtml += "</div>";
-		this.table = tableInHtml;
+  /**
+   * Get all properties from array of objects
+   *
+   * @param  {*} tableDataInJson data in json
+   * @return {Array.<String>}	array of properties
+   */
+  getAllObjectProperties(tableDataInJson) {
+    let properties = [];
+    for (let i = 0; i < tableDataInJson.length; i++) {
+      let objKeys = Object.keys(tableDataInJson[i]);
+      if (objKeys.length > 0) {
+        let objKeysData = Object.keys(tableDataInJson[i].data);
+        // union operation with properties and objKeysData
+        properties = [...new Set([...properties, ...objKeysData])];
+      }
+    }
+    return properties;
+  }
 
-	}
+  /**
+   * Build header part of table
+   *
+   * @param  {Object} propertyName    Object property as a header of table
+   * @param  {Json} tableDataInJson 	Data in json format
+   * @param  {Array.<Object>} properties 	All object properties
+   * @return {String}                 Table header as string
+   */
+  buildTableHeader(propertyName, tableDataInJson, properties) {
+    let TableMainHeader = '<thead><tr>';
 
-	/**
-	 * Get all properties from array of objects
-	 *
-	 * @param  {*} tableDataInJson data in json
-	 * @return {Array.<String>}	array of properties
-	 */
-	getAllObjectProperties(tableDataInJson) {
-		let properties = [];
-		for (let i = 0; i < tableDataInJson.length; i++) {
-			let objKeys = Object.keys(tableDataInJson[i]);
-			if (objKeys.length > 0) {
-				let objKeysData = Object.keys(tableDataInJson[i].data);
-				// union operation with properties and objKeysData
-				properties = [...new Set([...properties, ...objKeysData])];
-			}
-		}
-		return properties;
-	}
+    if (propertyName !== '') {
+      TableMainHeader += '<th>' + propertyName + '</th><tr>';
+    }
 
-	/**
-	 * Build header part of table
-	 *
-	 * @param  {Object} propertyName    Object property as a header of table
-	 * @param  {Json} tableDataInJson 	Data in json format
-	 * @param  {Array.<Object>} properties 	All object properties
-	 * @return {String}                 Table header as string
-	 */
-	 buildTableHeader(propertyName, tableDataInJson, properties){
-		let TableMainHeader = "<thead><tr>";
+    for (let i = 0; i < tableDataInJson.length; i++) {
+      // if object has some kids, set empty column, where cells will have image with arrow
+      if (Object.keys(tableDataInJson[i].kids).length > 0) {
+        TableMainHeader += '<th></th>';
+        break;
+      }
+    }
 
-		if (propertyName !== ""){
-			TableMainHeader += "<th>" + propertyName + "</th><tr>";
-		}
+    for (let k = 0; k < properties.length; k++) {
+      TableMainHeader += '<th>' + properties[k] + '</th>';
+    }
+    TableMainHeader += '</tr></thead>';
 
-		for (let i = 0; i < tableDataInJson.length; i++) {
-			// if object has some kids, set empty column, where cells will have image with arrow
-			if (Object.keys(tableDataInJson[i].kids).length > 0) {
-				TableMainHeader += "<th></th>";
-				break;
-			}
-		}
+    return TableMainHeader;
+  }
 
-		for (let k = 0; k < properties.length; k++) {
-			TableMainHeader += "<th>" + properties[k] + "</th>";
-		}
-		TableMainHeader += "</tr></thead>";
+  /**
+   * Build body part of table
+   *
+   * @param  {String} tableMainHeader Header part of the table
+   * @param  {Json} tableDataInJson Data in json format
+   * @param  {Array.<Object>} properties 	All object properties
+   * @return {String} Table body as string
+   */
+  buildTableBody(tableMainHeader, tableDataInJson, properties) {
+    // create body of table
+    let TableMainBody = '<tbody>';
+    for (let k = 0; k < tableDataInJson.length; k++) {
+      if (typeof tableDataInJson[k].data !== 'undefined') {
+        TableMainBody +=
+          '<tr id="' + this.tableIdString + '_row_' + parseInt(k, 10) + '" >';
 
-		return TableMainHeader;
-	}
+        if (tableMainHeader.indexOf('<th></th>') !== -1) {
+          if (Object.keys(tableDataInJson[k].kids).length > 0) {
+            TableMainBody += '<td><img src="' + ArrowImg + '"></td>';
+          } else {
+            TableMainBody += '<td></td>';
+          }
+        }
 
-	/**
-	 * Build body part of table
-	 *
-	 * @param  {String} tableMainHeader Header part of the table
-	 * @param  {Json} tableDataInJson Data in json format
-	 * @param  {Array.<Object>} properties 	All object properties
-	 * @return {String} Table body as string
-	 */
-	buildTableBody(tableMainHeader, tableDataInJson, properties){
-		// create body of table
-		let TableMainBody = "<tbody>";
-		for (let k = 0; k < tableDataInJson.length; k++) {
+        for (let l = 0; l < properties.length; l++) {
+          if (
+            Object.keys(tableDataInJson[k].data).indexOf(properties[l]) === -1
+          ) {
+            TableMainBody += '<td></td>';
+          } else {
+            TableMainBody +=
+              '<td>' + tableDataInJson[k].data[properties[l]] + '</td>';
+          }
+        }
 
-			if (typeof(tableDataInJson[k].data) !== 'undefined') {
+        TableMainBody += '</tr>';
+      }
+    }
+    TableMainBody += '</tbody>';
 
-				TableMainBody += "<tr id=\"" + this.tableIdString + "_row_" + parseInt(k, 10) + "\" >";
+    return TableMainBody;
+  }
 
-				if (tableMainHeader.indexOf("<th></th>") !== -1) {
-					if (Object.keys(tableDataInJson[k].kids).length > 0) {
-						TableMainBody += "<td><img src=\"" + ArrowImg + "\"></td>";
-					} else {
-						TableMainBody += "<td></td>";
-					}
-				}
-
-				for (let l = 0; l < properties.length; l++) {
-					if (Object.keys(tableDataInJson[k].data).indexOf(properties[l]) === -1) {
-						TableMainBody += "<td></td>";
-					} else {
-						TableMainBody += "<td>" + tableDataInJson[k].data[properties[l]] + "</td>";
-					}
-				}
-
-				TableMainBody += "</tr>";
-
-			}
-
-		}
-		TableMainBody += "</tbody>";
-
-		return TableMainBody;
-	}
-
-	/**
-	 * Build table header and body
-	 *
-	 * @param  {*} tableDataInJson   data in json
-	 * @return {string}              table header + body
-	 */
-	buildTableContent(tableDataInJson) {
-		let tableHeader = "";
-		let properties = {};
-		if (Array.isArray(tableDataInJson)){
-			properties = this.getAllObjectProperties(tableDataInJson);
-			tableHeader = "<table class=\"table table-striped table-bordered cell-border\" id=\"" + this.tableIdString + "\">" + this.buildTableHeader("", tableDataInJson, properties);
-			return tableHeader + this.buildTableBody(tableHeader, tableDataInJson, properties) + "</table>";
-		}
-	}
-
+  /**
+   * Build table header and body
+   *
+   * @param  {*} tableDataInJson   data in json
+   * @return {string}              table header + body
+   */
+  buildTableContent(tableDataInJson) {
+    let tableHeader = '';
+    let properties = {};
+    if (Array.isArray(tableDataInJson)) {
+      properties = this.getAllObjectProperties(tableDataInJson);
+      tableHeader =
+        '<table class="table table-striped table-bordered cell-border" id="' +
+        this.tableIdString +
+        '">' +
+        this.buildTableHeader('', tableDataInJson, properties);
+      return (
+        tableHeader +
+        this.buildTableBody(tableHeader, tableDataInJson, properties) +
+        '</table>'
+      );
+    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
+const webpackMerge = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 console.log("target event is " + TARGET);
 
-var outputFileName = "nested.datatables";
+var outputFileName = "nested.tables";
 outputFileName += (TARGET == 'prod' ? ".min.js" : ".js");
 
-module.exports = {
+const common = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: outputFileName,
-    library: 'lib',
+    library: 'nestedTables',
     libraryTarget:'umd',
     umdNamedDefine: true,
   },
@@ -42,3 +44,50 @@ module.exports = {
     })
   ]
 };
+
+if (TARGET === 'build' || !TARGET) {
+  module.exports = webpackMerge(common, {
+    devtool: 'source-map',
+    watchOptions: {
+      poll: true,
+    },
+  });
+}
+
+if (TARGET === 'prod' || !TARGET) {
+  module.exports = webpackMerge(common, {
+    plugins: [
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: true,
+        compress: {
+          warnings: false, // Suppress uglification warnings
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+        },
+        output: {
+          comments: false,
+        },
+        exclude: [/\.min\.js$/gi], // skip pre-minified libs
+      }),
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0,
+      }),
+    ],
+  });
+}
